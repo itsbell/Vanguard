fd2b62dfba59fa37193c311a6839908255e6baa3
sjyun <ehild97@gmail.com>
2026-08-03 19:59:56 +0900

    FEAT: ProjectileBullet Class 생성

---
+++ Source/Vanguard/ProjectileBullet.cpp
@@ -0,0 +1,6 @@
+#include "ProjectileBullet.h"
+
+AProjectileBullet::AProjectileBullet()
+{
+
+}
---
+++ Source/Vanguard/ProjectileBullet.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Projectile.h"
+#include "ProjectileBullet.generated.h"
+
+UCLASS()
+class VANGUARD_API AProjectileBullet : public AProjectile
+{
+	GENERATED_BODY()
+	
+public:
+	AProjectileBullet();
+};
